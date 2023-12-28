7b1e305c1507106da46d0fd7daec9978cdbbfdcc
Adrian Yevchuk <adrintik@gmail.com>
2023-12-28 20:23:01 +0200

    make build

---
+++ index.dac013a7.js
@@ -0,0 +1,3 @@
+var e=document.querySelector(".form"),n=document.querySelectorAll(".form__input"),t=function(){n.forEach(function(e){e.value=""})};e.addEventListener("submit",function(e){e.preventDefault(),t()});var o=document.querySelector(".page__body");window.addEventListener("hashchange",function(){"#menu"===window.location.hash?o.classList.add("page__body--with-menu"):o.classList.remove("page__body--with-menu")});//# sourceMappingURL=index.dac013a7.js.map
+
+//# sourceMappingURL=index.dac013a7.js.map
